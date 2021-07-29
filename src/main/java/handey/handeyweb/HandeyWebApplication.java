22511f169317369956abced1dfb77da3a66db63e
Hansle Lee <hssarah13@gmail.com>
2021-07-29 15:32:08 +0900

    Initial commit

---
+++ src/main/java/handey/handeyweb/HandeyWebApplication.java
@@ -0,0 +1,13 @@
+package handey.handeyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandeyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandeyWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/handey/handeyweb/HandeyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package handey.handeyweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandeyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
