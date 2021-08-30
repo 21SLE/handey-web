d004c7c474bf71fc3b490c58b73aab539d2397d7
Hansle Lee <hssarah13@gmail.com>
2021-08-31 01:06:36 +0900

    todo_box 테이블과 todo-elm테이블 조인!!!

--- src/main/java/com/handey/web/domain/home/ToDoBox.java
+++ src/main/java/com/handey/web/domain/home/ToDoBox.java
@@ -1,9 +1,25 @@
 package com.handey.web.domain.home;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@ToString(exclude = "toDoElmList")
 @Table(name = "todo_box")
+@DynamicInsert
+@DynamicUpdate
 public class ToDoBox {
 
     @Id @Column(name = "id") @GeneratedValue(strategy = GenerationType.IDENTITY) // db가 자동으로 ++해서 id 생성하는 것 = identity 전략
@@ -13,29 +29,41 @@ public class ToDoBox {
     private String title;
 
     @Column(name = "fixed")
+    @ColumnDefault("0")
     private boolean fixed;
 
-    public Long getId() {
-        return id;
-    }
+    @OneToMany(mappedBy = "toDoBox")
+    private List<ToDoElm> toDoElmList = new ArrayList<ToDoElm>();
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public boolean isFixed() {
-        return fixed;
-    }
-
-    public void setFixed(boolean fixed) {
-        this.fixed = fixed;
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public boolean isFixed() {
+//        return fixed;
+//    }
+//
+//    public void setFixed(boolean fixed) {
+//        this.fixed = fixed;
+//    }
+//
+//    public List<ToDoElm> getToDoElmList() {
+//        return toDoElmList;
+//    }
+//
+//    public void setToDoElmList(List<ToDoElm> toDoElmList) {
+//        this.toDoElmList = toDoElmList;
+//    }
 }
---
+++ src/main/java/com/handey/web/repository/home/ToDoElmRepository.java
@@ -0,0 +1,14 @@
+package com.handey.web.repository.home;
+
+import com.handey.web.domain.home.ToDoBox;
+import com.handey.web.domain.home.ToDoElm;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ToDoElmRepository {
+    ToDoElm save(ToDoElm ToDoElm);
+    Optional<ToDoElm> findById(Long id);
+    List<ToDoElm> findByToDoBoxId(Long toDoBoxId);
+    List<ToDoElm> findAll();
+}
