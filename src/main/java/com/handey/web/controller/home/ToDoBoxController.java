a82716c406a0b7f8bec2bcb2f26cdd60b33abad8
Hansle Lee <hssarah13@gmail.com>
2021-09-13 20:53:09 +0900

    creating todobox with filled todoelms api: for test

--- src/main/java/com/handey/web/controller/home/ToDoBoxController.java
+++ src/main/java/com/handey/web/controller/home/ToDoBoxController.java
@@ -2,6 +2,7 @@ package com.handey.web.controller.home;
 
 import com.handey.web.domain.home.ToDoBox;
 import com.handey.web.service.ToDoBoxService;
+import com.handey.web.service.ToDoElmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,11 +11,13 @@ import java.util.List;
 @RestController
 public class ToDoBoxController {
     private final ToDoBoxService toDoBoxService;
+    private final ToDoElmService toDoElmService;
 
                // 스프링 빈 등록 2가지 방법 중: 컴포넌트 스캔과 자동 의존관계 생성
     @Autowired // DI(Dependency Injection): Controller -> Service -> Repository
-    public ToDoBoxController(ToDoBoxService toDoBoxService) {
+    public ToDoBoxController(ToDoBoxService toDoBoxService, ToDoElmService toDoElmService) {
         this.toDoBoxService = toDoBoxService;
+        this.toDoElmService = toDoElmService;
     }
 
     /**
@@ -23,6 +26,26 @@ public class ToDoBoxController {
     @GetMapping("/toDoBoxList")
     public List<ToDoBox> getToDoBoxList() {
         return toDoBoxService.getToDoBoxList();
+    }
+
+    /**
+     * create toDoBox with todoElms only for tests
+     */
+    @PostMapping("/toDo")
+    public Long createToDoBox(@RequestBody ToDoParam param) {
+        Long toDoBoxId = toDoBoxService.createToDoBoxObj();
+        ToDoBoxParam toDoBoxParam = new ToDoBoxParam();
+        toDoBoxParam.setTitle(param.getTitle());
+        toDoBoxParam.setFixed(param.isFixed());
+        toDoBoxService.updateToDoBoxTitle(toDoBoxId, toDoBoxParam);
+        param.getToDoElmList().forEach(toDoElm -> {
+            Long toDoElmId = toDoElmService.createToDoElmObj(toDoBoxId);
+            ToDoElmParam toDoElmParam = new ToDoElmParam();
+            toDoElmParam.setContent(toDoElm.getContent());
+            toDoElmParam.setCompleted(toDoElm.isCompleted());
+            toDoElmService.updateToDoElmContent(toDoElmId, toDoElmParam);
+        });
+        return toDoBoxId;
     }
 
     /**
---
+++ src/main/java/com/handey/web/controller/home/ToDoParam.java
@@ -0,0 +1,15 @@
+package com.handey.web.controller.home;
+
+import com.handey.web.domain.home.ToDoElm;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ToDoParam {
+    private String title;
+
+    private boolean fixed;
+
+    private List<ToDoElm> toDoElmList;
+}
