bf8ad2a23ad252cdd4104d3152f3e4dc97ce95da
tbdpa <tbdpapdl@gmail.com>
2021-09-18 03:08:44 +0900

    오류 있어서 일단 마스터에서 삭제

--- src/main/java/com/handey/web/controller/history/WeeklyElmController.java
+++ src/main/java/com/handey/web/controller/history/WeeklyElmController.java
@@ -1,4 +1,14 @@
 package com.handey.web.controller.history;
 
+import com.handey.web.domain.history.WeeklyBox;
+import com.handey.web.service.WeeklyElmService;
+import com.handey.web.service.WeeklyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class WeeklyElmController {
+
 }
