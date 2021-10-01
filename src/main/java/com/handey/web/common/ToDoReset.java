a5bd1b78492d8ab8a05418fcf40f0b11a829c8bb
Hansle Lee <hssarah13@gmail.com>
2021-10-01 13:34:30 +0900

    todo 시간별 reset기능

--- src/main/java/com/handey/web/common/ToDoReset.java
+++ src/main/java/com/handey/web/common/ToDoReset.java
@@ -1,68 +1,95 @@
 package com.handey.web.common;
 
+import com.handey.web.domain.join.Member;
 import com.handey.web.service.ToDoBoxHstService;
 import com.handey.web.service.ToDoBoxService;
+import com.handey.web.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Component
 public class ToDoReset {
     private final ToDoBoxService toDoBoxService;
     private final ToDoBoxHstService toDoBoxHstService;
+    private final UserInfoService userInfoService;
 
     @Autowired
-    public ToDoReset(ToDoBoxService toDoBoxService, ToDoBoxHstService toDoBoxHstService) {
+    public ToDoReset(ToDoBoxService toDoBoxService, ToDoBoxHstService toDoBoxHstService, UserInfoService userInfoService) {
         this.toDoBoxService = toDoBoxService;
         this.toDoBoxHstService = toDoBoxHstService;
+        this.userInfoService = userInfoService;
     }
 
+    //user_info 테이블에서 reset_time == 0 인 user 것들만 todoBox 테이블에서 resetToDo함수 실행
     // 초, 분, 시, 일, 월, 요일
     @Scheduled(cron="0 0 0 * * *")
     @Transactional
     public void resetToDoAt00() {
-        resetToDo();
+        resetToDo("0");
     }
 
     @Scheduled(cron="0 0 01 * * *")
     @Transactional
     public void resetToDoAt01() {
-
+        resetToDo("1");
     }
 
     @Scheduled(cron="0 0 02 * * *")
     @Transactional
     public void resetToDoAt02() {
-
+        resetToDo("2");
     }
 
-    // fixedDelay마다 실행 5000 = 5초
-//    @Scheduled(fixedDelay=1000000)
+    @Scheduled(cron="0 0 03 * * *")
     @Transactional
-    public void resetToDo(){
-        System.out.println("resetToDo 테스트");
-        toDoBoxService.getToDoBoxList().forEach(toDoBox -> {
-            // history table로 복사
-            boolean doesToDoBoxNeedToBeDeleted = toDoBoxHstService.createToDoBoxHst(toDoBox.getMember(), toDoBox);
+    public void resetToDoAt03() {
+        resetToDo("3");
+    }
 
-            // todoelm 모두 completed이면 todobox 삭제
-            if(doesToDoBoxNeedToBeDeleted) {
-                toDoBoxService.deleteToDoBox(toDoBox.getId());
-            }
-        });
+    @Scheduled(cron="0 0 04 * * *")
+    @Transactional
+    public void resetToDoAt04() {
+        resetToDo("4");
+    }
+
+    @Scheduled(cron="0 0 05 * * *")
+    @Transactional
+    public void resetToDoAt05() {
+        resetToDo("5");
+    }
+
+    @Scheduled(cron="0 0 06 * * *")
+    @Transactional
+    public void resetToDoAt06() {
+        resetToDo("6");
+    }
+
+//    @Scheduled(fixedDelay=1000000)
+//    @Transactional
+//    public void resetToDoTest() {
+//        System.out.println("resetToDo 테스트");
+//        resetToDo("0");
+//    }
+
+    @Transactional
+    public void resetToDo(String resetTime){
+        // 0시엔 user_info 테이블에서 reset_time == 0 인 user 리스트 조회
+        List<Member> memberList = userInfoService.getUserListByResetTime(resetTime);
+
+        memberList.forEach(member ->
+                toDoBoxService.getToDoBoxListByUserId(member.getId()).forEach(toDoBox -> {
+                    // history로 복사
+                    boolean doesToDoBoxNeedToBeDeleted = toDoBoxHstService.createToDoBoxHst(toDoBox.getMember(), toDoBox);
+
+                    // todoelm 모두 completed이면 todobox 삭제
+                    if (doesToDoBoxNeedToBeDeleted) {
+                        toDoBoxService.deleteToDoBox(toDoBox.getId());
+                    }
+                }));
     }
 }
-
-// 1) 매월 10일 오전 11시
-//    0  1  1  10  *  *
-//
-// 2) 매일 오후 2시 5분 0초
-//    0  5  14  *  *  *
-//
-// 3) 10분마다 도는 스케줄러 : 10분 0초, 20분 0초...
-//    0  0/10  *  *  *
-//
-// 4) 조건에서만 실행되는 스케줄러 : 10분 0초, 11분 0초 ~ 15분 0초까지 실행
-//    0  10-15  *  *  *
