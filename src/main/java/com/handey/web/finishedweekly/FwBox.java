d9cc6e2ccf0de02a82250384c6cd0e919f4a7979
Hansle Lee <hssarah13@gmail.com>
2021-12-13 18:31:05 +0900

    fw box, fw elm

---
+++ src/main/java/com/handey/web/finishedweekly/FwBox.java
@@ -0,0 +1,53 @@
+package com.handey.web.finishedweekly;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.handey.web.member.Member;
+import com.handey.web.weekly.WeeklyBox;
+import com.handey.web.weekly.WeeklyElm;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "fw_box")
+@DynamicInsert
+@DynamicUpdate
+public class FwBox {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "savedt")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDate saveDt;
+
+    @JsonIgnore
+    @OneToOne
+    @JoinColumn(name = "weekly_box_id")
+    private WeeklyBox weeklyBox;
+
+    @JsonIgnore
+    @OneToOne
+    @JoinColumn(name = "userId")
+    private Member member;
+
+    @OneToMany(mappedBy = "fwBox")  //한 title이 여러 elm을 소지해도 된다. 양방향 관계의 주체가 되는 것은 weeklyBox
+    @JsonManagedReference
+    private List<FwElm> fwElmList = new ArrayList<>();
+}
---
+++ src/main/java/com/handey/web/finishedweekly/FwElm.java
@@ -0,0 +1,39 @@
+package com.handey.web.finishedweekly;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.handey.web.member.Member;
+import com.handey.web.weekly.WeeklyBox;
+import com.handey.web.weekly.WeeklyElm;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "fw_elm")
+@DynamicInsert
+@DynamicUpdate
+public class FwElm {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name="fw_box_id")
+    @JsonBackReference  //양방향 관계에서 종
+    private FwBox fwBox;
+
+    @JsonIgnore
+    @OneToOne
+    @JoinColumn(name = "weekly_elm_id")
+    private WeeklyElm weeklyElm;
+}
