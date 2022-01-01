901282c157a0d4415e2e8fcca5c771251772d2f7
Hansle Lee <hssarah13@gmail.com>
2022-01-01 17:29:07 +0900

    history 단건조회 api

--- src/main/java/com/handey/web/history/HistoryController.java
+++ src/main/java/com/handey/web/history/HistoryController.java
@@ -3,16 +3,19 @@ package com.handey.web.history;
 import com.handey.web.afterhistory.AfterHistoryService;
 import com.handey.web.common.response.ListResponse;
 import com.handey.web.common.response.ResponseService;
+import com.handey.web.common.response.SingleResponse;
 import com.handey.web.finishedweekly.FwBox;
 import com.handey.web.finishedweekly.FwService;
 import com.handey.web.todohistory.ToDoBoxHst;
 import com.handey.web.todohistory.ToDoBoxHstService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -63,5 +66,20 @@ public class HistoryController {
         });
         List<History> hstList = new ArrayList<>(hstMap.values());
         return responseService.returnListResponse(hstList);
+    }
+
+    /**
+     * 회원 투두 + 애프터 히스토리 박스 하나 단건 조회
+     */
+    @GetMapping("/user/{userId}/history/date")
+    public SingleResponse<History> getHistoryBoxByUserIdAndDate(@PathVariable Long userId, @RequestParam String dt) {
+        LocalDate searchDt = LocalDate.parse(dt, DateTimeFormatter.ISO_DATE);
+        History historyBox = new History();
+        List<ToDoBoxHst> toDoBoxHstList = toDoBoxHstService.getToDoBoxHstListByUserIdAndDate(userId, searchDt);
+        List<FwBox> fwBoxHstList = fwService.getFwBoxListByUserIdAndDate(userId, searchDt);
+        historyBox.setSaveDt(searchDt);
+        historyBox.setToDoBoxHstList(toDoBoxHstList);
+        historyBox.setFwBoxList(fwBoxHstList);
+        return responseService.returnSingleResponse(historyBox);
     }
 }
